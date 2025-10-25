95f9541bc28ee085c0d6e52abd0b0c17999146a6
vinayak.sulegai <vinayak.sulegai@outlook.com>
2025-10-25 18:08:22 +0530

    sub deploy

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "demo" {
+  name     = "rg-demo"
+  location = "East US"
+}
+ 
---
+++ sub/main.tf
@@ -0,0 +1,51 @@
+##########################################################
+# Terraform to create Azure RG and VNet (free resources)
+# Region: South India
+# Cost: ₹0 under Pay-as-you-Go (no paid compute/storage)
+##########################################################
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # Optionally specify subscription if multiple are configured:
+  # subscription_id = "<your-subscription-id>"
+}
+
+# --------------------------
+# Resource Group
+# --------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "Viny-RG-IND-01"
+  location = "South India"
+
+  tags = {
+    Environment = "FreeTier"
+    Owner       = "Viny"
+    CostCenter  = "ZeroCost"
+  }
+}
+
+# --------------------------
+# Virtual Network
+# --------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Vnet-IND-01"
+  address_space       = ["172.100.10.0/27"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    Environment = "FreeTier"
+    Owner       = "Viny"
+  }
+}
